6123a8c88381e50b7e272954709bebf46b98b173
@Andrea.Berti <ablsonsempremi@gmail.com>
2023-03-29 17:54:06 +0200

    Parziale mossa del Parry

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,10 +6,24 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public CharacterController controller;
+    public Transform cam;
 
     public float speed = 6f;
+    public float time = 2f;
 
-    
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    public Transform player;
+    public Vector3 rotateAmount;
+    public bool isStop;
+
+    private void Start()
+    {
+        isStop = true;
+    }
+
+                 
 
     void Update()
     {
@@ -17,15 +31,54 @@ public class PlayerMovement : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
+         
+
         if (direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;           
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+
+            isStop = false;
+
+            time = 2f;
             
-            controller.Move(direction * speed * Time.deltaTime);
+        }
+        else
+        {
+            isStop = true;
+            
         }
 
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            Parry();
+
+        }
+
+
 
     }
+
+   
+
+
+    public void Parry()
+    {
+        if (isStop == true && time > 0f)
+        {
+            player.Rotate(rotateAmount * Time.deltaTime);
+            time -= Time.deltaTime;
+        }
+
+
+    }
+
+    
 }
+
+
